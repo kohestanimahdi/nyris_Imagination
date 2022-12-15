bfad8142f4ba7ff7fb98230d05f42202ce402686
mahdi kohestani <kohestanimahdi@gmail.com>
2022-12-15 21:55:29 +0330

    add the service class for convert the image

---
+++ src/Imagination.Server/Services/ImageConverter/ImageConverterService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Imagination.Services.ImageConverter
+{
+    public class ImageConverterService
+    {
+        public async Task<byte[]> ConvertImageToJPEGAsync()
+        {
+
+        }
+    }
+}
