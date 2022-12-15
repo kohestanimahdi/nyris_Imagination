3a55bc72d397de8c456b90ce08e76d67646212b7
mahdi kohestani <kohestanimahdi@gmail.com>
2022-12-15 22:52:52 +0330

    use the service in contreoller to convert the file to JPEG format

---
+++ src/Imagination.Server/Controllers/ImageConverterController.cs
@@ -0,0 +1,53 @@
+﻿using Imagination.Services.ImageConverter;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Imagination.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageConverterController : ControllerBase
+    {
+        private readonly IImageConverterService _imageConverter;
+
+        public ImageConverterController(IImageConverterService imageConverter)
+        {
+            _imageConverter = imageConverter ?? throw new ArgumentNullException(nameof(imageConverter));
+        }
+
+
+        /// <summary>
+        /// Convert the image file that is get from the stream body to JPEG format
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("/convert")]
+        public async Task<IActionResult> ConvertImageToJPEGAsync(CancellationToken cancellationToken)
+        {
+            byte[] resultFile = null;
+            // the image may post as a form file, so we have to consider both
+            if (Request.HasFormContentType && Request.Form?.Files is { Count: > 0 })
+            {
+                // consider the first file of the form
+                resultFile = _imageConverter.GetImageAsJPEGFormat(Request.Form.Files.First());
+            }
+            else
+            {
+                using var memoryStream = new MemoryStream();
+
+                // if the data is sended in stream, we have to read all the stream
+                await Request.BodyReader.CopyToAsync(memoryStream, cancellationToken);
+
+                resultFile = _imageConverter.GetImageAsJPEGFormat(memoryStream);
+            }
+
+            // return the byte array of the file to the cliend with content header image/jpeg
+            return File(resultFile, System.Net.Mime.MediaTypeNames.Image.Jpeg);
+        }
+    }
+}
