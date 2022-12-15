9d5a31c93a4a430ea0dd798c4f4de25d0158e8c8
mahdi kohestani <kohestanimahdi@gmail.com>
2022-12-15 21:55:51 +0330

    add cutom exception in application in order to modify the response type

---
+++ src/Imagination.Server/Exceptions/ImageConverterException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Imagination.Exceptions
+{
+    public class ImageConverterException : Exception
+    {
+        public enum ImageConverterExceptionType : string
+        {
+            a = "a",
+
+        }
+        public ImageConverterException(string message) : base(message)
+        {
+        }
+    }
+}
