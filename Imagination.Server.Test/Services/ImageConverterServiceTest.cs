114e427d093debec90b6fce1fec6c151702e00d2
mahdi kohestani <kohestanimahdi@gmail.com>
2022-12-16 10:06:09 +0330

    add test project in the solution in order to test the image converter service

---
+++ Imagination.Server.Test/Services/ImageConverterServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Imagination.Exceptions;
+using Imagination.Services.ImageConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imagination.Server.Test.Services
+{
+    public class ImageConverterServiceTest
+    {
+        private readonly IImageConverterService _imageConverterService;
+        public ImageConverterServiceTest()
+        {
+            _imageConverterService = new ImageConverterService();
+        }
+
+        [Fact]
+        public void GetImageAsJPEGFormat_ErrorStreamIsNull()
+        {
+            MemoryStream memoryStream = null;
+            var exception = Assert.Throws<ImageConverterException>(() => _imageConverterService.GetImageAsJPEGFormat(memoryStream));
+
+            Assert.Equal(ImageConverterExceptionType.EmptyStream, exception.Type);
+        }
+
+        [Fact]
+        public void GetImageAsJPEGFormat_ErrorStreamIsEmpty()
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            var exception = Assert.Throws<ImageConverterException>(() => _imageConverterService.GetImageAsJPEGFormat(memoryStream));
+
+            Assert.Equal(ImageConverterExceptionType.EmptyStream, exception.Type);
+        }
+    }
+}
